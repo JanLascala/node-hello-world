0c38a0858adcf49f358b2bad5bbe2fcc866f1724
JanLascala <lascala.jan@gmail.com>
2025-03-11 15:32:09 +0100

    Bonus works

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("hello world")
 
 console.log("hello boolean")
+
+//bonus? 
+process.argv[0]
+process.argv[1]
+process.argv[2]
+
+const textFromTerminal = process.argv[2];
+console.log(textFromTerminal)
